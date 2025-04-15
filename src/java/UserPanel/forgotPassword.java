61fc932b26bbb9f34acc1c82169e77d964a16392
Deepti Mahisare <deeptimahisare@gmail.com>
2025-04-15 21:45:41 +0530

    updates

--- src/java/UserPanel/forgotPassword.java
+++ src/java/UserPanel/forgotPassword.java
@@ -32,15 +32,7 @@ import javax.servlet.http.HttpSession;
 @WebServlet(name = "forgotPassword", urlPatterns = {"/forgotPassword"})
 public class forgotPassword extends HttpServlet {
 
-    /**
-     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
-     * methods.
-     *
-     * @param request servlet request
-     * @param response servlet response
-     * @throws ServletException if a servlet-specific error occurs
-     * @throws IOException if an I/O error occurs
-     */
+  
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         response.setContentType("text/html;charset=UTF-8");
